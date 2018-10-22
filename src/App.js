c7895c3dae30bf4471268558e6dad4ff2d1932d8
Only <lifedamara@163.com>
2018-10-22 16:39:44 +0800

    initial dir

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+
+import logo from './assets/images/logo.svg';
 import './App.css';
 
 class App extends Component {
