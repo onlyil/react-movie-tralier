21087ffededb58273c341361e27684a2d43070a6
Only <lifedamara@163.com>
2018-10-31 16:38:03 +0800

    Feat: add sidebar layout

--- src/components/sidebar/sidebar.js
+++ src/components/sidebar/sidebar.js
@@ -1,10 +1,20 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import './sidebar.scss'
+
 
 export default class Sidebar extends Component {
+  static propTypes = {
+    menu: PropTypes.array.isRequired,
+  }
+
   render() {
+    const { menu } = this.props
     return (
       <div>
-        侧边栏
+        {menu.map(item =>
+          <div className="menu-item">{item.title}</div>  
+        )}
       </div>
     )
   }
--- src/pages/front/home/home.js
+++ src/pages/front/home/home.js
@@ -1,14 +1,38 @@
 import React, { Component } from 'react'
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom'
+import Axios from 'axios'
 import Sidebar from '@/components/sidebar/sidebar'
 import './home.scss'
 
 export default class home extends Component {
+  state = {
+    menu: [
+      {
+        title: '2018',
+      },
+      {
+        title: '2017',
+      },
+      {
+        title: '2016',
+      },
+    ]
+  }
+
+  componentDidMount() {
+    // this.network().getYears()
+  }
+
+  network = () => ({
+    getYears: async () => {},
+  })
+
   render() {
+    const { menu } = this.state
     return (
       <div className="home-container">
         <div className="side-container">
-          <Sidebar />
+          <Sidebar menu={menu} />
         </div>
         <div className="main-container">
           展示
